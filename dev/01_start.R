ff1dc62ce77945ebb19fb95b334e6daebe930a8a
Javier Gracia Tabuenca <javier.graciatabuenca@tuni.fi>
2022-09-28 13:43:53 +0300

    Initial commit

--- dev/01_start.R
+++ dev/01_start.R
@@ -13,35 +13,18 @@
 
 renv::init()
 
-# ## Create Common Files ----
-# ## See ?usethis for more information
-# #usethis::use_mit_license( "Golem User" )  # You can set another license here
-# usethis::use_readme_rmd( open = FALSE )
-# #usethis::use_code_of_conduct()
-# usethis::use_lifecycle_badge( "Experimental" )
-# usethis::use_news_md( open = FALSE )
-#
+usethis::use_description(
+  fields = list(
+    Title = "HadesExtras",
+    Description = "Functions to complement Hades packages",
+    `Authors@R` = 'person("Javier", "Gracia-Tabuenca", email = "javier.graciatabuenca@tuni.fi",
+                          role = c("aut", "cre"),
+                          comment = c(ORCID = "0000-0002-2455-0598"))',
+    License = "none",
+    Language =  "en"
+  )
+)
+
 # ## Use git ----
-# usethis::use_git()
-#
-# ## Init Testing Infrastructure ----
-# ## Create a template for tests
-# golem::use_recommended_tests()
-#
-# ## Use Recommended Packages ----
-# golem::use_recommended_deps()
-#
-# ## Favicon ----
-# # If you want to change the favicon (default is golem's one)
-# golem::use_favicon() # path = "path/to/ico". Can be an online file.
-# golem::remove_favicon()
-#
-# ## Add helper functions ----
-# golem::use_utils_ui()
-# golem::use_utils_server()
-#
-# # You're now set! ----
-#
-# # go to dev/02_dev.R
-# rstudioapi::navigateToFile( "dev/02_dev.R" )
-#
+usethis::use_git()
+usethis::use_github(private = TRUE)
